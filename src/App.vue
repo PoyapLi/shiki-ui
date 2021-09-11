74b6b103a0752d8bb3bcb1651f1283543afd7b92
Poyap <ryougiako@gmail.com>
2021-09-11 19:28:23 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Poyap/>
+</template>
+
+<script>
+import Poyap from './components/Poyap.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Poyap
+  }
+}
+</script>
---
+++ src/components/Poyap.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>我的组件</div>
+</template>
