cf1a7a42845d5f3f0da1ea9dd7ee684e5e9f154f
Poyap <ryougiako@gmail.com>
2021-09-12 21:15:16 +0800

    支持 level 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -18,14 +18,19 @@ export default {
     size: {
       type: String,
       default: 'normal',
+    },
+    level: {
+      type: String,
+      default: 'normal',
     }
   },
   setup(props) {
-    const {theme, size} = props;
+    const {theme, size, level} = props;
     const classes = computed(() => {
       return {
         [`shiki-theme-${theme}`]: theme,
         [`shiki-size-${size}`]: size,
+        [`shiki-level-${level}`]: size,
       };
     });
     return {classes};
@@ -41,6 +46,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 .shiki-button {
   box-sizing: border-box;
   height: $h;
@@ -55,6 +61,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
 
   & + & {
     margin-left: 8px;
@@ -93,16 +100,73 @@ $radius: 4px;
       background: darken(white, 5%);
     }
   }
-  &.shiki-theme-button{
-    &.shiki-size-big{
-      font-size: 24px;
-      height: 48px;
-      padding: 0 16px;
+
+  &.shiki-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+
+  &.shiki-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+
+  &.shiki-theme-button {
+    &.shiki-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
     }
-    &.shiki-size-small{
-      font-size: 12px;
-      height: 20px;
-      padding: 0 4px;
+
+    &.shiki-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.shiki-theme-link {
+    &.shiki-level-danger {
+      color: $red;
+
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.shiki-theme-text {
+    &.shiki-level-main {
+      color: $blue;
+
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+
+    &.shiki-level-danger {
+      color: $red;
+
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
     }
   }
 }
