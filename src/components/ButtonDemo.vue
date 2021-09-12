e51eeb6e8c9f363bae16a816ba353d256dc57a9f
Poyap <ryougiako@gmail.com>
2021-09-12 19:52:47 +0800

    添加 theme 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -4,7 +4,16 @@
   </div>
   <h1>示例</h1>
   <div>
-    <Button>你好，这里是插槽的内容</Button>
+    <Button>你好</Button>
+    <Button theme="button">
+      你好
+    </Button>
+    <Button theme="link">
+      你好
+    </Button>
+    <Button theme="text">
+      你好
+    </Button>
   </div>
 </template>
 
@@ -12,7 +21,13 @@
 import Button from '../lib/Button.vue';
 
 export default {
-  components: {Button}
+  components: {Button},
+  setup(){
+    const onClick = ()=>{
+      console.log('你好');
+    }
+    return {onClick}
+  }
 };
 
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,10 +1,25 @@
 <template>
-  <button>
+  <button class="shiki-button"
+    :class="`theme-${theme}`"
+  >
     <slot/>
   </button>
 </template>
 
 <script lang="ts">
-
+export default {
+  props: {
+    theme:{
+      type: String,
+      default: 'button',
+    }
+  }
+};
 
 </script>
+
+<style lang="scss" scoped>
+div {
+  border: 1px solid red;
+}
+</style>
