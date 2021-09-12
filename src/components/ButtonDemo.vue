a786b5870e2b3377d2d1a8f894386400bcb5c56c
Poyap <ryougiako@gmail.com>
2021-09-12 20:56:40 +0800

    支持 size 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,18 +2,18 @@
   <div>
     button 示例
   </div>
-  <h1>示例</h1>
+  <h1>示例1</h1>
   <div>
     <Button>你好</Button>
-    <Button theme="button">
-      你好
-    </Button>
-    <Button theme="link">
-      你好
-    </Button>
-    <Button theme="text">
-      你好
-    </Button>
+    <Button theme="button">你好</Button>
+    <Button theme="link">你好</Button>
+    <Button theme="text">你好</Button>
+  </div>
+  <h1>示例2</h1>
+  <div>
+    <Button size="big">大大大</Button>
+    <Button>普通</Button>
+    <Button size="small">小小小</Button>
   </div>
 </template>
 
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,77 +1,109 @@
 <template>
   <button class="shiki-button"
-    :class="{[`shiki-theme-${theme}`]: theme}"
+          :class="classes"
   >
     <slot/>
   </button>
 </template>
 
 <script lang="ts">
+import {computed} from 'vue';
+
 export default {
   props: {
-    theme:{
+    theme: {
       type: String,
       default: 'button',
+    },
+    size: {
+      type: String,
+      default: 'normal',
     }
+  },
+  setup(props) {
+    const {theme, size} = props;
+    const classes = computed(() => {
+      return {
+        [`shiki-theme-${theme}`]: theme,
+        [`shiki-size-${size}`]: size,
+      };
+    });
+    return {classes};
   }
 };
 
 </script>
 
-<style lang="scss" >
+<style lang="scss">
 
-  $h: 32px;
-  $border-color: #d9d9d9;
-  $color: #333;
-  $blue: #40a9ff;
-  $radius: 4px;
-  .shiki-button {
-    box-sizing: border-box;
-    height: $h;
-    padding: 0 12px;
-    cursor: pointer;
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    white-space: nowrap;
-    background: white;
-    color: $color;
-    border: 1px solid $border-color;
-    border-radius: $radius;
-    box-shadow: 0 1px 0 fade-out(black, 0.95);
+$h: 32px;
+$border-color: #d9d9d9;
+$color: #333;
+$blue: #40a9ff;
+$radius: 4px;
+.shiki-button {
+  box-sizing: border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
 
-    & + & {
-      margin-left: 8px;
-    }
+  & + & {
+    margin-left: 8px;
+  }
 
-    &:hover,
-    &:focus {
-      color: $blue;
-      border-color: $blue;
-    }
+  &:hover,
+  &:focus {
+    color: $blue;
+    border-color: $blue;
+  }
 
-    &:focus {
-      outline: none;
-    }
+  &:focus {
+    outline: none;
+  }
 
-    &::-moz-focus-inner {
-      border: 0;
-    }
-    &.shiki-theme-link{
-      border-color: transparent;
-      box-shadow: none;
-      color: $blue;
-      &:hover,&:focus{
-        color: lighten($blue, 10%);
-      }
-    }
-    &.shiki-theme-text {
-      border-color: transparent;
-      box-shadow: none;
-      color: inherit;
-      &:hover, &:focus{
-        background: darken(white, 5%);
-      }
+  &::-moz-focus-inner {
+    border: 0;
+  }
+
+  &.shiki-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+
+    &:hover, &:focus {
+      color: lighten($blue, 10%);
     }
   }
+
+  &.shiki-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+
+    &:hover, &:focus {
+      background: darken(white, 5%);
+    }
+  }
+  &.shiki-theme-button{
+    &.shiki-size-big{
+      font-size: 24px;
+      height: 48px;
+      padding: 0 16px;
+    }
+    &.shiki-size-small{
+      font-size: 12px;
+      height: 20px;
+      padding: 0 4px;
+    }
+  }
+}
 </style>
