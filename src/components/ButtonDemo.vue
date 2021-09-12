b325a3fbc8d5798df3eef16c8b0bd2ec59761df3
Poyap <ryougiako@gmail.com>
2021-09-12 18:04:34 +0800

    创建 Button 组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,47 +1,18 @@
 <template>
   <div>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-
+    button 示例
+  </div>
+  <h1>示例</h1>
+  <div>
+    <Button>你好，这里是插槽的内容</Button>
   </div>
 </template>
+
+<script lang="ts">
+import Button from '../lib/Button.vue';
+
+export default {
+  components: {Button}
+};
+
+</script>
