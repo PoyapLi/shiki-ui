88046dbfc585bf73ab0927a1dd3ac3a6c3472f49
Poyap <ryougiako@gmail.com>
2021-09-12 20:46:44 +0800

    支持 theme 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -57,5 +57,21 @@ export default {
     &::-moz-focus-inner {
       border: 0;
     }
+    &.shiki-theme-link{
+      border-color: transparent;
+      box-shadow: none;
+      color: $blue;
+      &:hover,&:focus{
+        color: lighten($blue, 10%);
+      }
+    }
+    &.shiki-theme-text {
+      border-color: transparent;
+      box-shadow: none;
+      color: inherit;
+      &:hover, &:focus{
+        background: darken(white, 5%);
+      }
+    }
   }
 </style>
