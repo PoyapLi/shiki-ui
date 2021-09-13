2ae3becab2b0335e9dd579f7b30a2b52ae56a6c5
Poyap <ryougiako@gmail.com>
2021-09-13 20:18:12 +0800

    Dialog 支持自定义 title 和 content

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -3,10 +3,12 @@
     <div class="shiki-dialog-overlay" @click="closeOnClickOverlay"></div>
     <div class="shiki-dialog-wrapper">
       <div class="shiki-dialog">
-        <header>标题<span @click="close" class="shiki-dialog-close"></span></header>
+        <header>
+          <slot name="title"/>
+          <span @click="close" class="shiki-dialog-close"></span>
+        </header>
         <main>
-          <p>第一行</p>
-          <p>第二行</p>
+          <slot name="content" />
         </main>
         <footer>
           <Button level="main" @click="ok">OK</Button>
@@ -21,8 +23,8 @@
 import Button from './Button.vue';
 
 export default {
-  props:{
-    visible:{
+  props: {
+    visible: {
       type: Boolean,
       default: false,
     },
@@ -30,33 +32,33 @@ export default {
       type: Boolean,
       default: true,
     },
-    ok:{
+    ok: {
       type: Function,
     },
-    cancel:{
-      type:Function,
+    cancel: {
+      type: Function,
     }
   },
   components: {Button},
-  setup(props, context){
-    const close = ()=>{
-      context.emit('update:visible', false)
-    }
-    const onClickOverlay = ()=>{
-      if(props.closeOnClickOverlay){
-        close()
+  setup(props, context) {
+    const close = () => {
+      context.emit('update:visible', false);
+    };
+    const onClickOverlay = () => {
+      if (props.closeOnClickOverlay) {
+        close();
       }
-    }
-    const ok = ()=>{
-      if(props.ok && props.ok() !== false){
-        close()
+    };
+    const ok = () => {
+      if (props.ok && props.ok() !== false) {
+        close();
       }
-    }
-    const cancel = ()=>{
-      context.emit('cancel')
-      close()
-    }
-    return {close, onClickOverlay, ok, cancel}
+    };
+    const cancel = () => {
+      context.emit('cancel');
+      close();
+    };
+    return {close, onClickOverlay, ok, cancel};
   }
 };
 
@@ -72,6 +74,7 @@ $border-color: #d9d9d9;
   box-shadow: 0 0 3px fade_out(black, 0.5);
   min-width: 15em;
   max-width: 90%;
+
   &-overlay {
     position: fixed;
     top: 0;
@@ -81,6 +84,7 @@ $border-color: #d9d9d9;
     background: fade_out(black, 0.5);
     z-index: 10;
   }
+
   &-wrapper {
     position: fixed;
     left: 50%;
@@ -88,7 +92,8 @@ $border-color: #d9d9d9;
     transform: translate(-50%, -50%);
     z-index: 11;
   }
-  >header {
+
+  > header {
     padding: 12px 16px;
     border-bottom: 1px solid $border-color;
     display: flex;
@@ -96,20 +101,24 @@ $border-color: #d9d9d9;
     justify-content: space-between;
     font-size: 20px;
   }
-  >main {
+
+  > main {
     padding: 12px 16px;
   }
-  >footer {
+
+  > footer {
     border-top: 1px solid $border-color;
     padding: 12px 16px;
     text-align: right;
   }
+
   &-close {
     position: relative;
     display: inline-block;
     width: 16px;
     height: 16px;
     cursor: pointer;
+
     &::before,
     &::after {
       content: '';
@@ -120,9 +129,11 @@ $border-color: #d9d9d9;
       top: 50%;
       left: 50%;
     }
+
     &::before {
       transform: translate(-50%, -50%) rotate(-45deg);
     }
+
     &::after {
       transform: translate(-50%, -50%) rotate(45deg);
     }
