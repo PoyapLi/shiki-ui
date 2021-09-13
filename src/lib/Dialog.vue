f8293f46fdb6e42d53a33655ec886c4b8ccbdb17
Poyap <ryougiako@gmail.com>
2021-09-13 20:55:53 +0800

    使用了具名插槽和 Teleport

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,21 +1,23 @@
 <template>
   <template v-if="visible">
-    <div class="shiki-dialog-overlay" @click="closeOnClickOverlay"></div>
-    <div class="shiki-dialog-wrapper">
-      <div class="shiki-dialog">
-        <header>
-          <slot name="title"/>
-          <span @click="close" class="shiki-dialog-close"></span>
-        </header>
-        <main>
-          <slot name="content" />
-        </main>
-        <footer>
-          <Button level="main" @click="ok">OK</Button>
-          <Button @click="cancel">Cancel</Button>
-        </footer>
+    <Teleport to="body">
+      <div class="shiki-dialog-overlay" @click="closeOnClickOverlay"></div>
+      <div class="shiki-dialog-wrapper">
+        <div class="shiki-dialog">
+          <header>
+            <slot name="title"/>
+            <span @click="close" class="shiki-dialog-close"></span>
+          </header>
+          <main>
+            <slot name="content" />
+          </main>
+          <footer>
+            <Button level="main" @click="ok">OK</Button>
+            <Button @click="cancel">Cancel</Button>
+          </footer>
+        </div>
       </div>
-    </div>
+    </Teleport>
   </template>
 </template>
 
