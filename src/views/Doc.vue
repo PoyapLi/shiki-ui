517ea878b964af70cb2170617f27ad12e8b8c309
Poyap <ryougiako@gmail.com>
2021-09-15 16:38:12 +0800

    填充文档内容

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -52,6 +52,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
 .layout {
   display: flex;
   flex-direction: column;
@@ -83,7 +84,7 @@ export default {
   aside {
     background: lightblue;
     width: 150px;
-    padding: 16px;
+    padding: 16px 0;
     position: fixed;
     top: 0;
     left: 0;
@@ -91,11 +92,19 @@ export default {
     height: 100%;
     > h2 {
       margin-bottom: 4px;
+      padding: 0 16px;
     }
 
     > ol {
       > li {
-        padding: 4px 0;
+        > a {
+          display: block;
+          padding: 4px 16px;
+          text-decoration: none;
+        }
+        .router-link-active {
+          background: white;
+        }
       }
     }
   }
