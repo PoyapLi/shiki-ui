155a786193559c152a32b12830270a6daa3012c5
Poyap <ryougiako@gmail.com>
2021-09-15 15:58:16 +0800

    更新列表切换按钮样式

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -6,11 +6,13 @@
       </svg>
     </div>
     <ul class="menu">
-      <li>菜单1</li>
-      <li>菜单2</li>
+      <li>
+        <router-link to="/doc">文档</router-link>
+      </li>
     </ul>
-    <span class="toggleAside" @click="toggleMenu">
-    </span>
+    <svg v-if="toggleMenuButtonVisible" class="toggleAside" @click="toggleMenu">
+      <use xlink:href="#icon-menu"></use>
+    </svg>
   </div>
 </template>
 
@@ -18,6 +20,12 @@
 import {inject,Ref} from 'vue';
 
 export default {
+  props:{
+    toggleMenuButtonVisible:{
+      type: Boolean,
+      default: false,
+    }
+  },
   setup(){
     const menuVisible = inject<Ref<boolean>>('menuVisible')
     const toggleMenu = () => {
@@ -58,12 +66,13 @@ export default {
   > .toggleAside {
     width: 24px;
     height: 24px;
-    background: red;
     position: absolute;
     left: 16px;
     top: 50%;
     transform: translateY(-50%);
     display: none;
+    background: fade-out(black, 0.9);
+    border-radius: 4px;
   }
   @media (max-width: 500px){
     > .menu{display: none}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,39 +13,95 @@
     </div>
   </div>
   <div class="features">
-    <svg class="icon">
-      <use xlink:href="#icon-Vue"></use>
-    </svg>
-    <svg class="icon">
-      <use xlink:href="#icon-ts"></use>
-    </svg>
-    <svg class="icon">
-      <use xlink:href="#icon-light"></use>
-    </svg>
+    <ul>
+      <li>
+        <svg class="icon">
+          <use xlink:href="#icon-Vue"></use>
+        </svg>
+        <h3>基于 Vue 3</h3>
+        <p>使用了 Vue 3 Composition API</p>
+      </li>
+      <li>
+        <svg class="icon">
+          <use xlink:href="#icon-ts"></use>
+        </svg>
+        <h3>基于 TypeScript </h3>
+        <p>源代码采用 TypeScript 书写（非严格检查）</p>
+      </li>
+      <li>
+        <svg class="icon">
+          <use xlink:href="#icon-light"></use>
+        </svg>
+        <h3>代码易读</h3>
+        <p>每个组件的源代码都极其简洁</p>
+      </li>
+    </ul>
+
+
   </div>
 </template>
 
 <script lang="ts">
-import Topnav from "../components/Topnav.vue";
+import Topnav from '../components/Topnav.vue';
+
 export default {
   components: {Topnav},
-}
+};
 </script>
 
 <style lang="scss" scoped>
 $border-radius: 16px;
-$blue:#1f7d99;
+$blue: #1f7d99;
 $color: #367182;
-.topnavAndBanner{
-  background: linear-gradient(135deg, rgba(148,231,233,1) 0%, rgba(88,185,214,1) 100%);
+.topnavAndBanner {
+  background: linear-gradient(135deg, rgba(148, 231, 233, 1) 0%, rgba(88, 185, 214, 1) 100%);
   clip-path: ellipse(80% 60% at 50% 40%);
 }
+
 .features {
-  > svg {
-    width: 64px;
-    height: 64px;
+  margin: 64px auto;
+  width: 400px;
+  @media (min-width: 800px) {
+    width: 800px;
+  }
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+
+  > ul {
+    display: flex;
+    flex-wrap: wrap;
+
+    > li {
+      width: 400px;
+      margin: 16px 0;
+      display: grid;
+      justify-content: start;
+      align-content: space-between;
+      grid-template-areas:
+        "icon title"
+        "icon text";
+      grid-template-columns: 80px auto;
+      grid-template-rows: 1fr auto;
+
+      > svg {
+        grid-area: icon;
+        width: 64px;
+        height: 64px;
+      }
+
+      > h3 {
+        grid-area: title;
+        font-size: 28px;
+      }
+
+      > p {
+        grid-area: text
+      }
+    }
   }
 }
+
 .banner {
   color: $color;
   padding: 100px 0;
@@ -53,11 +109,13 @@ $color: #367182;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  > .actions{
+
+  > .actions {
     padding: 8px 0;
+
     a {
-      margin:0 8px;
-      background:$blue;
+      margin: 0 8px;
+      background: $blue;
       color: white;
       display: inline-block;
       padding: 8px 24px;
